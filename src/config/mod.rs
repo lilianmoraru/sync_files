3a52173fc4c247b15aba018736924a312399c018
Lilian A. Moraru <lilian.moraru90@gmail.com>
2017-07-01 20:10:22 +0000

    Add target SSH connection settings

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -3,14 +3,23 @@ use std::collections::HashMap;
 #[derive(Deserialize, Debug)]
 struct Config {
     default: String,
-    sync: HashMap<String, Sync>
+    sync: HashMap<String, Sync>,
+    target: HashMap<String, Target>
 }
 
 #[derive(Deserialize, Debug)]
 struct Sync {
+    targets: Option<Vec<Target>>,
     files: Option<Vec<String>>,
     dependencies: Option<Vec<String>>,
     symlink_rootfs: Option<String>,
     pre_sync_cmd: Option<String>,
     post_sync_cmd: Option<String>
+}
+
+#[derive(Deserialize, Debug)]
+struct Target {
+    address:  String,
+    user:     String,
+    password: String
 }
